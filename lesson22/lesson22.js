9138d062a15714201109fe22106c591b51beae70
Iden851 <ha150685idj@gmail.com>
2022-10-11 22:29:22 +0300

    1

--- lesson22/lesson22.js
+++ lesson22/lesson22.js
@@ -1,3 +1,4 @@
+// Создать страницу где будет отображен счетчик и кнопки инкремента и декремента этого счетчика.
 function counterFunc(count) {
     let up = count.querySelector(".up");
     let down = count.querySelector(".down");
@@ -18,3 +19,35 @@ function counterFunc(count) {
 let counts = document.querySelectorAll(".counter");
 
 counts.forEach(counterFunc);
+
+// Получить несколькими способами доступ к элементу с Важной информацией и всем
+// элементам с женскими именами.
+
+// выделим цветом "woman"
+let woman1 = document.querySelectorAll("ul > li");
+for (let elem of woman1) {
+    if (elem.dataset.id == "woman") {
+        elem.style.background = "lime";
+    }
+}
+
+// выводим в консоль имена "woman"
+let woman2 = document.getElementsByTagName("li");
+for (let elem of woman2) {
+    if (elem.dataset.id === "woman") {
+        console.log(elem.innerHTML);
+    }
+}
+
+// ищем елемент с "Важной информацией" и выделяем цветом
+
+let impotantInfo = document.querySelectorAll("div");
+for (let elem of impotantInfo) {
+    if (elem.textContent === "Важная информация") {
+        elem.style.background = "grey";
+    }
+}
+
+// ищем елемент с "Важной информацией" и выводим содержимое в консоль
+let impotantInfo1 = document.querySelector("div");
+console.log(impotantInfo1.innerHTML);
