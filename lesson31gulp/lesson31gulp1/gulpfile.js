df1b9d1349544407f0126518c6b4fce08a1fdf02
Iden851 <ha150685idj@gmail.com>
2022-11-11 22:27:25 +0200

    1

---
+++ lesson31gulp/lesson31gulp1/gulpfile.js
@@ -0,0 +1,53 @@
+const { src, dest, series, parallel, watch } = require("gulp");
+const del = require("del");
+const browserSync = require("browser-sync").create();
+
+const babel = require("gulp-babel");
+const origin = "src";
+const destination = "build";
+
+async function clean(cb) {
+    await del(destination);
+    cb();
+}
+
+function html(cb) {
+    src(`${origin}/*.html`).pipe(dest(destination));
+    cb();
+}
+
+function css(cb) {
+    src(`${origin}/*.css`).pipe(dest(destination));
+    cb();
+}
+
+function js(cb) {
+    src(`${origin}/*.js`)
+        .pipe(
+            babel({
+                presets: ["@babel/env"],
+            })
+        )
+        .pipe(dest(destination));
+    cb();
+}
+
+function watcher(cb) {
+    watch(`${origin}/*.html`).on("change", series(html, browserSync.reload));
+    watch(`${origin}/*.css`).on("change", series(css, browserSync.reload));
+    watch(`${origin}/*.js`).on("change", series(js, browserSync.reload));
+    cb();
+}
+
+function server(cb) {
+    browserSync.init({
+        notify: false,
+        open: false,
+        server: {
+            baseDir: destination,
+        },
+    });
+    cb();
+}
+
+exports.default = series(clean, parallel(html, css, js), server, watcher);
---
+++ lesson31gulp/lesson31gulp1/src/index.js
@@ -0,0 +1,17 @@
+phoneNumberVar2.onfocus = function () {
+    if (this.classList.contains("succesfull")) {
+        this.classList.remove("succesfull");
+    } else if (this.classList.contains("invalid")) {
+        this.classList.remove("invalid");
+    }
+};
+
+phoneNumberVar2.onblur = function () {
+    let regexp = /^\+380[5-9]\d{8}$/;
+
+    if (!regexp.test(phoneNumberVar2.value)) {
+        this.classList.add("invalid");
+    } else {
+        this.classList.add("succesfull");
+    }
+};
